a4ff898b3e28c1fa146c5bb7e06df026ef470593
Johan Hake <johan.hake@gmail.com>
2020-11-04 22:08:58 +0100

    La til ting om arrays

---
+++ Kapittel 6-12/mugshots/elevnavn.js
@@ -0,0 +1,89 @@
+let elevNavn = [
+    ["Eskild", "Kamfjord", "3STE"],
+    ["Felix", "Anthonisen", "3STA"],
+    ["Fredrik", "Henriksen", "3STE"],
+    ["Helena", "Larsen", "3STD"],
+    ["Ingvild", "Sandven", "3STB"],
+    ["Magnus", "Alstad", "3STE"],
+    ["Marius", "Effersø", "3STC"],
+    ["Markus", "Nord", "3STB"],
+    ["Michael", "Eilertsen", "3STA"],
+    ["Oskar", "Lothe", "3STE"],
+    ["Sam", "Kanaan", "3STD"],
+    ["Sigurd", "Dynna", "3STC"],
+    ["Sondre", "Bærø", "3STA"],
+    ["Vetle", "Svendsen", "3STC"]
+];
+
+let elevObjekt = [
+    {
+        "fornavn": "Eskild",
+        "etternavn": "Kamfjord",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Felix",
+        "etternavn": "Anthonisen",
+        "klasse": "3STA"
+    },
+    {
+        "fornavn": "Fredrik",
+        "etternavn": "Henriksen",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Helena",
+        "etternavn": "Larsen",
+        "klasse": "3STD"
+    },
+    {
+        "fornavn": "Ingvild",
+        "etternavn": "Sandven",
+        "klasse": "3STB"
+    },
+    {
+        "fornavn": "Magnus",
+        "etternavn": "Alstad",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Marius",
+        "etternavn": "Effersø",
+        "klasse": "3STC"
+    },
+    {
+        "fornavn": "Markus",
+        "etternavn": "Nord",
+        "klasse": "3STB"
+    },
+    {
+        "fornavn": "Michael",
+        "etternavn": "Eilertsen",
+        "klasse": "3STA"
+    },
+    {
+        "fornavn": "Oskar",
+        "etternavn": "Lothe",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Sam",
+        "etternavn": "Kanaan",
+        "klasse": "3STD"
+    },
+    {
+        "fornavn": "Sigurd",
+        "etternavn": "Dynna",
+        "klasse": "3STC"
+    },
+    {
+        "fornavn": "Sondre",
+        "etternavn": "Bærø",
+        "klasse": "3STA"
+    },
+    {
+        "fornavn": "Vetle",
+        "etternavn": "Svendsen",
+        "klasse": "3STC"
+    }
+];
